642ff45651f3e5f38963363d5531bb15c053873d
Kyalo460 <Kyalo460@gmail.com>
2024-09-05 18:38:39 +0300

    pushing files

--- 0x00-Webpack/task_1/js/dashboard_main.js
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -1,19 +1,19 @@
-import $ from 'jQuery'
-import _ from 'lodash'
+import $ from 'jquery';
+import debounce from 'lodash/debounce';
 
 
-const count = 0
-
+let count = 0;
 $('body').append('<p>Holberton Dashboard</p>');
 $('body').append('<p>Dashboard data for the students</p>');
 $('body').append('<button>Click here to get started</button>');
-$('body').append("<p id='count'></p>");
+$('body').append('<p id="count"></p>');
 $('body').append('<p>Copyright - Holberton School</p>');
 
 function updateCounter() {
   count += 1;
-
-  $('#count').html(`<p id="count">${count} clicks on the button</p>`);
+  $('#count').html(`${count} clicks on the button`);
 }
 
-$('button').click(_.debounce(updateCounter, 200));
+const debouncedCounter = debounce(updateCounter, 500);
+
+document.getElementsByTagName('button')[0].addEventListener('click', debouncedCounter);
--- 0x00-Webpack/task_1/webpack.config.js
+++ 0x00-Webpack/task_1/webpack.config.js
@@ -1,10 +1,17 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  mode: 'production',
   entry: './js/dashboard_main.js',
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'public')
+    filename: 'bundle.js', // The name of the output JS file
+    path: path.resolve(__dirname, 'public'), // Output folder (./public)
   },
-  mode: 'production'
-}
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html', // Use the HTML file as a template
+      filename: 'index.html',       // Output filename for the HTML file in ./public
+    }),
+  ]
+};
