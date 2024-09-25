9cdab2df131472ca3a329370807efbfa4d4e7e8e
Kyalo460 <Kyalo460@gmail.com>
2024-09-25 08:51:08 +0300

    used semistandard

--- 0x06-React_Immutable/4-mutations.js
+++ 0x06-React_Immutable/4-mutations.js
@@ -1,4 +1,4 @@
-import {Map} from 'immutable';
+import { Map } from 'immutable';
 
 export const map = Map(
   {
@@ -7,8 +7,8 @@ export const map = Map(
     3: 'Elijah',
     4: 'Oliver',
     5: 'Jacob',
-    6: 'Lucas',
+    6: 'Lucas'
   }
-)
+);
 
-export const map2 = map.set('2', 'Benjamin').set('4', 'Oliver')
+export const map2 = map.set('2', 'Benjamin').set('4', 'Oliver');
--- 0x06-React_Immutable/8-seq.js
+++ 0x06-React_Immutable/8-seq.js
@@ -1,13 +1,13 @@
 import { Seq } from 'immutable';
 
-export default function printBestStudents(object) {
+export default function printBestStudents (object) {
   const seqObj = Seq(object);
 
   const transformed = seqObj.filter(value => value.score < 70).map(value => {
-    value.firstName = value.firstName.charAt(0).toUpperCase() + value.firstName.slice(1)
-    value.lastName = value.lastName.charAt(0).toUpperCase() + value.lastName.slice(1)
+    value.firstName = value.firstName.charAt(0).toUpperCase() + value.firstName.slice(1);
+    value.lastName = value.lastName.charAt(0).toUpperCase() + value.lastName.slice(1);
     return value;
-  })
+  });
 
   console.log(transformed.toObject);
 }
